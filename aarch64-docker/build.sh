506943c6245c11bca899066d3965c2f8310f41f5
Aris Tzoumas <atzoum@gmail.com>
2017-01-15 19:49:56 +0200

    fixes for docker build

--- aarch64-docker/build.sh
+++ aarch64-docker/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -x
-rm -rf /workspace/*
-git clone --depth 1 --branch v${1} https://github.com/docker/docker.git /workspace
-cd workspace
+git clone --depth 1 --branch v${1} https://github.com/docker/docker.git /docker-build-workspace
+cd docker-build-workspace
 make tgz
