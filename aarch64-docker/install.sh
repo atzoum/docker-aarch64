dc49f16ff155e78eed34cd5ebce52d519dbf949d
Aris Tzoumas <atzoum@gmail.com>
2017-01-22 00:10:13 +0200

    add some logging

--- aarch64-docker/install.sh
+++ aarch64-docker/install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 DOCKER_VERSION=1.13.0 && \
+echo "stopping docker service" && \
 systemctl stop docker.service && \
-wget https://dl.dropboxusercontent.com/u/3207856/docker-${DOCKER_VERSION}.tgz -P /tmp/ && \
+echo "downloading docker-${DOCKER_VERSION}.tgz to /tmp/" && \
+wget -q https://dl.dropboxusercontent.com/u/3207856/docker-${DOCKER_VERSION}.tgz -P /tmp/ && \
+echo "extracting binaries" && \
 tar -xvf /tmp/docker-${DOCKER_VERSION}.tgz -C /usr/bin/ --strip-components=1 && \
+echo "removing temporary files" && \
 rm /tmp/docker-${DOCKER_VERSION}.tgz && \
+echo "starting docker service" && \
 systemctl daemon-reload && \
-systemctl start docker.service
+systemctl start docker.service && \
+echo "enjoy!"
