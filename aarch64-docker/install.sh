23faae317ee1ab67d6cde40e7d566c55f3080b46
atzoum <atzoum@gmail.com>
2017-03-04 22:08:22 +0200

    Update install.sh

--- aarch64-docker/install.sh
+++ aarch64-docker/install.sh
@@ -3,7 +3,7 @@ DOCKER_VERSION=17.03.0-ce && \
 echo "stopping docker service" && \
 systemctl stop docker.service && \
 echo "downloading docker-${DOCKER_VERSION}.tgz to /tmp/" && \
-wget -q https://dl.dropboxusercontent.com/u/3207856/docker-${DOCKER_VERSION}.tgz -P /tmp/ && \
+wget -q https://github.com/atzoum/docker-aarch64/releases/download/docker-${DOCKER_VERSION}/docker-${DOCKER_VERSION}.tgz -P /tmp/ && \
 echo "extracting binaries" && \
 tar -xvf /tmp/docker-${DOCKER_VERSION}.tgz -C /usr/bin/ --strip-components=1 && \
 echo "removing temporary files" && \
